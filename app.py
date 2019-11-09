ac46c3ac3e69f7e430258faf64cc73930c5b4184
wheel405 <54213700+wheel405@users.noreply.github.com>
2019-11-09 12:43:48 -0600

    Add files via upload
    uploaded starter code, app.py with imports, and resources folder from the instructions provided in the classrepo

---
+++ app.py
@@ -0,0 +1,43 @@
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+
+
+if __name__ == '__main__':
+    app.run()
